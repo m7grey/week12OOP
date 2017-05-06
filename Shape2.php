91d6b7701ba8b14053e257f89ecda2a1c6e71811
m7grey <m7grey@gmail.com>
2017-05-06 15:08:05 -0400

    class Square extends Rectangle

--- Shape2.php
+++ Shape2.php
@@ -16,8 +16,7 @@ class Circle extends Shape
 
     function __construct($radius)
     {
-$this->radius = $radius;
-        echo "I am alive";
+        $this->radius = $radius;
     }
 
     function getArea()
@@ -28,14 +27,14 @@ $this->radius = $radius;
 
 class Rectangle extends Shape
 {
-private $height = 1;
-private $width = 1;
+    private $height = 1;
+    private $width = 1;
 
 
-    function __construct($width, $height) {
-    $this->width= $width;
-    $this->height = $height;
-        echo "I am alive ";
+    function __construct($width, $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
     }
 
     function getArea()
@@ -45,14 +44,39 @@ private $width = 1;
 
 }
 
+class Triangle extends Rectangle
+{
+    function getArea()
+    {
+        return parent::getArea();
+    }
+}
+
+class Square extends Rectangle
+{
+    function __construct($len)
+    {
+        parent::__construct($len, $len);
+    }
+}
+
 $myShape = new Circle(10);
-print_r($myShape->getArea());
+echo "The area for Circle " . get_class($myShape) . "is" . $myShape->getArea();
 echo "<hr>";
+
 $myShape2 = new Circle(5);
-print_r($myShape2->getArea());
+echo "The area for Circle " . get_class($myShape2) . "is" . $myShape2->getArea();
 echo "<hr>";
-$myShape3 = new Rectangle(2,3);
-print_r($myShape3->getArea());
+
+$myShape3 = new Rectangle(2, 3);
+echo "The area for Rectangle " . get_class($myShape3) . "is" . $myShape3->getArea();
+echo "<hr>";
+
+$myShape4 = new Triangle(5, 6);
+echo "The area for Triangle " . get_class($myShape4) . "is" . $myShape4->getArea();
+echo "<hr>";
+
+$myShape5 = new Square(6);
+echo "The area for Square " . get_class($myShape5) . "is" . $myShape5->getArea();
 
 ?>
-
