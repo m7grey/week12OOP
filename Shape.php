5935d0bace18871f812e73325a8bdfd63d6e4342
m7grey <m7grey@gmail.com>
2017-05-06 13:45:21 -0400

    changes Shape-> Circle.

--- Shape.php
+++ Shape.php
@@ -2,18 +2,31 @@
 
 //Filename: Shape.php
 
-class Shape
+abstract class Shape
 {
     function getArea()
     {
-return 0;
+        return 0;
     }
 }
-class Circle extends Shape{
 
+class Circle extends Shape
+{
+    private $radius = 1;
+
+    function __construct($radius)
+    {
+        echo "I am alive";
+    }
+
+    function getArea()
+    {
+        return $this->radius * $this->radius * pi();
+    }
 }
-$myshape = new Shape();
-print_r($myshape);
+
+$myshape = new Circle(10);
+print_r($myshape->getArea());
 
 ?>
 
